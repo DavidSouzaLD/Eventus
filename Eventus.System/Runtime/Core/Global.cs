93d4586698ac9d43c17e5b16929dde01647e23ca
David Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2025-07-06 13:02:59 -0300

    first

---
+++ Eventus.System/Runtime/Core/Global.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventus.Runtime.Core
+{
+    public static class Global
+    {
+        public const string DEFAULT_CATEGORY = "Any";
+        public const string DOC_EN_URL = "https://drive.google.com/file/d/1Im80gGSRjI3wQk58spIdw8Hk3WV8e7vS/view?usp=sharing";
+        public const string DOC_PT_URL = "https://drive.google.com/file/d/1_eTNkZ_QXpK8Im-p9NjtiJwRhQ3kDzye/view?usp=sharing";
+    }
+}
